896431ee44ac2186002ff776255ceee84092e4e6
Eduardo Mayen <ed_mayen@hotmail.com>
2022-07-15 22:39:35 -0500

    Bug fix & searching pokemon

--- app/src/main/java/com/example/pokedex/pokemonlist/PokemonListScreen.kt
+++ app/src/main/java/com/example/pokedex/pokemonlist/PokemonListScreen.kt
@@ -41,7 +41,8 @@ import com.google.accompanist.coil.CoilImage
 
 @Composable
 fun PokemonListScreen(
-    navController: NavController
+    navController: NavController,
+    viewModel: PokemonListViewModel = hiltNavGraphViewModel()
 ) {
     Surface(
         color = MaterialTheme.colors.background,
@@ -62,7 +63,9 @@ fun PokemonListScreen(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp)
-            )
+            ) {
+                viewModel.searchPokemonList(it)
+            }
 
             Spacer(modifier = Modifier.height(16.dp))
             PokemonList(navController = navController)
@@ -100,7 +103,7 @@ fun SearchBar(
                 .background(Color.White)
                 .padding(horizontal = 20.dp, vertical = 12.dp)
                 .onFocusChanged {
-                    isHintDisplayed = it != FocusState.Active
+                    isHintDisplayed = it != FocusState.Active && text.isNotEmpty()
                 }
         )
         if (isHintDisplayed) {
@@ -131,6 +134,9 @@ fun PokemonList(
     val isLoading by remember {
         viewModel.isLoading
     }
+    val isSearching by remember {
+        viewModel.isSearching
+    }
 
     LazyColumn(contentPadding = PaddingValues(16.dp)) {
         val itemCount = if (pokemonList.size % 2 == 0) {
@@ -139,7 +145,7 @@ fun PokemonList(
             pokemonList.size / 2 + 1
         }
         items(itemCount) {
-            if (it >= itemCount - 1 && !endReached) {
+            if (it >= itemCount - 1 && !endReached && !isLoading && !isSearching) {
                 viewModel.loadPokemonPaginated()
             }
             PokedexRow(rowIndex = it, entries = pokemonList, navController = navController)
